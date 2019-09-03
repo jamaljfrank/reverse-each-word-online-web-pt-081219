f0ecafa0677eed54025330b170d62e397a2681f6
Jamal Frank <jamaljfrank@gmail.com>
2019-09-03 02:44:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ end
 
 def reverse_each_word(string)
   new_array = string.split(" ")
-  reverse_array = []
+  reverse_array = new_array.collect do |word|
   
     reverse_array.push(word.reverse)
 end
