877fa35c70284c2f00efef2eb7364b2495f1abc2
Jamal Frank <jamaljfrank@gmail.com>
2019-09-03 02:38:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,16 @@ def reverse_each_word(string)
   reverse_array = []
   new_array.each do |word|
     reverse_array.push(word.reverse)
-  end
+end
   return reverse_array.join(" ")
 end
 
 def reverse_each_word(string)
-  
+  new_array = string.split(" ")
+  reverse_array = []
+  new_array.collect do |word|
+    reverse_array.push(word.reverse)
+end
+  return reverse_array.join(" ")
   
   
